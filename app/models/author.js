6bd9815f599a9a8491bd76f279ce3676cb16ba5f
Tadej Novak <tadej@tano.si>
2015-08-24 15:56:21 +0200

    Display authors from backend

---
+++ app/models/author.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  first_name: DS.attr(),
+  last_name: DS.attr(),
+
+  name: Ember.computed('first_name', 'last_name', function() {
+    return this.get('first_name') + ' ' + this.get('last_name');
+  })
+});

eb02cee213cac90759e955473c751663a4f9d784
Tadej Novak <tadej@tano.si>
2016-11-26 16:13:31 +0100

    WIP

--- app/components/report-modal.js
+++ app/components/report-modal.js
@@ -1,13 +1,27 @@
 import Ember from 'ember';
+import ENV from 'fmf/config/environment';
 
 export default Ember.Component.extend({
+  session: Ember.inject.service('session'),
+
   actions: {
     approveModal: function() {
-      let type = this.$.find('input[name=item-type]').val();
-      let number = this.$.find('input[name=item-number]').val();
-      let content = this.$.find('input[name=item-content]').val();
+      const type = this.$('input[name=item-type]').val();
+      const number = this.$('input[name=item-number]').val();
+      const content = this.$('input[name=item-content]').val();
 
       console.log(type, number, content);
+
+      this.get('session').authorize('authorizer:oauth2', (headerName, headerValue) => {
+        const data = {
+          title: 'Title',
+          content: '1234567890 1234567890'
+        };
+
+        const headers = {};
+        headers[headerName] = headerValue;
+        Ember.$.ajax(ENV.APP.backendUri + '/api/pm', { headers: headers, method: 'POST', data: data });
+      });
     }
   }
 });
